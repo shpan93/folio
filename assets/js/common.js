158b477440512ade24450b2f72a622c2a6b07ed1
anton_shpilenya <anton.sh@brights.com.ua>
2016-05-22 17:50:56 +0300

    working

---
+++ assets/js/common.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+popups.init();
+
+});
+
+
+var popups = (function() {
+    var _self = {};
+    var $items = $('#projects .item');
+    var $popup = $('.popup');
+    _self.position = {};
+    
+    _self.init = function () {
+        _self.bindEvents();
+    }
+    _self.setPopupRect = function(){
+        $popup.css(_self.position)
+        console.log(_self.position)
+    }
+    _self.bindEvents=function () {
+        $items.on('click',function () {
+            _self.position = {
+                left:$(this).offset().left,
+                top:$(this).offset().top - $(document).scrollTop(),
+                width:$(this)[0].getBoundingClientRect().width.toFixed(3),
+                height:$(this)[0].getBoundingClientRect().height.toFixed(3)
+            };
+            _self.setPopupRect();
+        });
+    };
+    return _self;
+})()
---
+++ gulpfile.js
@@ -0,0 +1,86 @@
+/// <vs />
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var sass = require('gulp-sass');
+var reload = browserSync.reload;
+var gutil = require('gulp-util');
+//var inject = require('gulp-inject');
+var plumber = require('gulp-plumber');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var spritesmith = require('gulp.spritesmith');
+
+var onError = function (err) {
+    gutil.beep();
+    console.log(err);
+};
+
+var src = {
+    scss: 'assets/sass/**/*.scss',
+    css: 'assets/css',
+    html: ['Views/**/*.cshtml', '*.html'],
+    js: 'assets/js/**/*.js',
+    map: 'assets/map',
+};
+
+//gulp.task('jsadd', function () {
+//    var target = gulp.src('./src/index.html');
+//    // It's not necessary to read the files (will speed up things), we're only after their paths:
+//    var sources = gulp.src(['assets/js/lib/*.js'], {read: false});
+//
+//    return target.pipe(inject(sources))
+//        .pipe(gulp.dest('./src'));
+//});
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function () {
+
+    browserSync({
+        startPath: "/",
+        proxy: "localhost:58726"
+    });
+
+    gulp.watch(src.scss, ['sass']);
+    gulp.watch(src.html).on('change', reload);
+    gulp.watch(src.js).on('change', reload);
+});
+
+// Compile sass into CSS
+gulp.task('sass', function () {
+    return gulp.src(src.scss)
+               .pipe(plumber())
+        .pipe(sourcemaps.init())
+       
+        .pipe(sass.sync({
+            outputStyle: 'compressed',
+        }))
+         .pipe(autoprefixer({
+             browsers: ['last 50 versions'],
+             cascade: false
+         }))
+        .pipe(sourcemaps.write(src.map))
+        .pipe(gulp.dest(src.css))
+
+        .pipe(browserSync.stream());
+});
+gulp.task('autoprefix', function () {
+    return gulp.src(src.css)
+         .pipe(autoprefixer({
+             browsers: ['last 50 versions'],
+             cascade: false
+         }))
+});
+
+
+
+gulp.task('sprite', function () {
+    var spriteData = gulp.src('./assets/images/section-icons/*.png').pipe(spritesmith({
+        imgName: 'section-icons-sprite.png',
+        cssName: 'section-icons-sprite.css',
+      // algorithm: 'left-right',
+        algorithmOpts: { sort: false }
+    }));
+    return spriteData.pipe(gulp.dest('./assets/images/section-icons/'));
+});
+
+gulp.task('default', ['serve']);
